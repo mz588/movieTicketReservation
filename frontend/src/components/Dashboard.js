be9d8807c0e612dcdce535f97c39220cd4255d70
Chenxi Qian <cq53@cornell.edu>
2022-04-23 20:27:38 -0400

    Finish dashboard front-end

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,20 +1,23 @@
 import "./bootstrap.css"
 import {
   selectUserName,
+  selectUserEmail,
   selectUserPhoto,
   setUserLoginDetails,
   setSignOutState,
   selectUserReservations,} from "../features/user/userSlice";
 import { useDispatch, useSelector } from "react-redux";
+
 const Dashboard = (props) => {
   const userName = useSelector(selectUserName);
   const reservations = useSelector(selectUserReservations);
+  const userEmail = useSelector(selectUserEmail)
   const reserved = []
   const reservedForDisplay = []
   reservations.forEach(a => reserved.push(a.array))
   reserved.map(function(reservation) {
     reservedForDisplay.push(<div className="card" key={reservation}>
-    <div className="card-body" >
+    <div className="card-body">
     <div className="flex flex-column align-items-center text-left">
         <div className="row">
         <h2 className="d-flex align-items-center text-info mb-5">🎟 Movie Ticket 🎟</h2>
@@ -62,9 +65,8 @@ const Dashboard = (props) => {
                 <div className="d-flex flex-column align-items-center text-center">
                   <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Admin" className="rounded-circle" width="150"/>
                   <div className="mt-3">
-                    <h3>John Doe</h3>
-                    <p className="text-secondary mb-1">Full Stack Developer</p>
-                    <p className="text-muted font-size-sm">Bay Area, San Francisco, CA</p>
+                    <h3>{userName}</h3>
+                    {/* <p className="text-secondary mb-1">Moto</p> */}
                   </div>
                 </div>
               </div>
@@ -76,12 +78,12 @@ const Dashboard = (props) => {
                 <div className="flex flex-column align-items-center text-left">
                   <div className="row">
                     <div className="col-sm-3"><h6 className="mb-0">Name</h6></div>
-                    <div className="col-sm-9 text-secondary">Kenneth Valdez</div>
+                    <div className="col-sm-9 text-secondary">{userName}</div>
                   </div>
                   <hr/>
                   <div className="row">
                     <div className="col-sm-3"><h6 className="mb-0">Email</h6></div>
-                    <div className="col-sm-9 text-secondary">fip@jukmuh.al</div>
+                    <div className="col-sm-9 text-secondary">{userEmail}</div>
                   </div>
                   <hr/>
                   <div className="row">
@@ -96,7 +98,7 @@ const Dashboard = (props) => {
                   <hr/>
                   <div className="row">
                     <div className="col-sm-3"><h6 className="mb-0">Address</h6></div>
-                    <div className="col-sm-9 text-secondary">Bay Area, San Francisco, CA</div>
+                    <div className="col-sm-9 text-secondary">116 Hoy Rd, Ithaca, NY, 14853</div>
                   </div>
                   <hr/>
                 </div>
@@ -105,96 +107,6 @@ const Dashboard = (props) => {
 
             {/* Reserved Movies Starts Here */}
             {reservedForDisplay}
-
-            <div className="card">
-              <div className="card-body">
-              <div className="flex flex-column align-items-center text-left">
-                  <div className="row">
-                  <h2 className="d-flex align-items-center text-info mb-5" >🎟 Movie Ticket 🎟</h2>
-                  </div>
-                  <div className="row">
-                    <div className="col-sm-3"><h6 className="mb-0">Title</h6></div>
-                    <div className="col-sm-9 text-secondary">Frozon II</div>
-                  </div>
-                  <hr/>
-                  <div className="row">
-                    <div className="col-sm-3"><h6 className="mb-0">Theatre</h6></div>
-                    <div className="col-sm-9 text-secondary">Ithaca Movie Center</div>
-                  </div>
-                  <hr/>
-                  <div className="row">
-                    <div className="col-sm-3"><h6 className="mb-0">Date</h6></div>
-                    <div className="col-sm-9 text-secondary">May 28, 2022</div>
-                  </div>
-                  <hr/>
-                  <div className="row">
-                    <div className="col-sm-3"><h6 className="mb-0">Time</h6></div>
-                    <div className="col-sm-9 text-secondary">14:45 - 17:00</div>
-                  </div>
-                  <hr/>
-                </div>
-              </div>
-            </div>
-
-            <div className="card">
-              <div className="card-body">
-                <div className="flex flex-column align-items-center text-left">
-                  <div className="row">
-                    <h2 className="d-flex align-items-center text-info mb-5" style={h2Style}>🎟 Movie Ticket 🎟</h2>
-                  </div>
-                  <div className="row">
-                    <div className="col-sm-3"><h6 className="mb-0">Title</h6></div>
-                    <div className="col-sm-9 text-secondary">Frozon II</div>
-                  </div>
-                  <hr/>
-                  <div className="row">
-                    <div className="col-sm-3"><h6 className="mb-0">Theatre</h6></div>
-                    <div className="col-sm-9 text-secondary">Ithaca Movie Center</div>
-                  </div>
-                  <hr/>
-                  <div className="row">
-                    <div className="col-sm-3"><h6 className="mb-0">Date</h6></div>
-                    <div className="col-sm-9 text-secondary">May 28, 2022</div>
-                  </div>
-                  <hr/>
-                  <div className="row">
-                    <div className="col-sm-3"><h6 className="mb-0">Time</h6></div>
-                    <div className="col-sm-9 text-secondary">14:45 - 17:00</div>
-                  </div>
-                  <hr/>
-                </div>
-              </div>
-            </div>
-
-            <div className="card">
-              <div className="card-body">
-                <div className="flex flex-column align-items-center text-left">
-                  <div className="row">
-                    <h2 className="d-flex align-items-center text-info mb-5">🎟 Movie Ticket 🎟</h2>
-                  </div>
-                  <div className="row">
-                    <div className="col-sm-3"><h6 className="mb-0">Title</h6></div>
-                    <div className="col-sm-9 text-secondary">Frozon II</div>
-                  </div>
-                  <hr/>
-                  <div className="row">
-                    <div className="col-sm-3"><h6 className="mb-0">Theatre</h6></div>
-                    <div className="col-sm-9 text-secondary">Ithaca Movie Center</div>
-                  </div>
-                  <hr/>
-                  <div className="row">
-                    <div className="col-sm-3"><h6 className="mb-0">Date</h6></div>
-                    <div className="col-sm-9 text-secondary">May 28, 2022</div>
-                  </div>
-                  <hr/>
-                  <div className="row">
-                    <div className="col-sm-3"><h6 className="mb-0">Time</h6></div>
-                    <div className="col-sm-9 text-secondary">14:45 - 17:00</div>
-                  </div>
-                  <hr/>
-                </div>
-              </div>
-            </div>
 
           </div>
         </div>
